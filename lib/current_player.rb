032b30df72c41176bc82e5658858c770e6219b8f
jasmine Magdi <jesojolay@yahoo.com>
2017-09-16 14:41:13 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,5 +1,11 @@
 def turn_count(board)
-
+    counter = 0
+board.each do |spot|
+    if board[color] != " " || board[color] != "" || board[color] != nil
+        counter += 1
+    end
+end
+return counter
 end
 
 def current_player(board)
