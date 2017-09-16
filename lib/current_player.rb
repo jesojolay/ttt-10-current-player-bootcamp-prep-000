83101545f247d2b0bcf326491836b617db89c92d
jasmine Magdi <jesojolay@yahoo.com>
2017-09-16 15:03:25 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -11,7 +11,7 @@ end
 def current_player(board)
     if turn_count(board) == 0
         return "X"
-    elsif turn_count(board) % 2 == 0
+    elsif turn_count(board).even?
         return "O"
     end
 end
